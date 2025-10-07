5d29612f820e4f0db5337085da591f0fe433f243
yosoycalde <juancalderon2005.18@gmail.com>
2025-10-07 15:25:03 -0500

    actualizado

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 include "index.html"
 ?>
+
+<?php
+include "admin.js"
+?>
+
+<?php
+include "index.js"
+?>
+
+<?php
+include "coneccion.php"
+?>
+
+<?php
+include "config.php"
+?>
+
+<?php
+include "consultar_tickets.php"
+?>
+
+<?php
+include "detalles_ticket.php"
+?>
+
+<?php
+include "procesar_tickets.php"
+?>
